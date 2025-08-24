641dfd5f503120ecf789412f669abccda2a0cac2
tasneemu-13 <168448640+tasneemu-13@users.noreply.github.com>
2025-08-24 21:33:52 +0530

    Update HomePage.js

--- attendease_frontend/src/pages/HomePage.js
+++ attendease_frontend/src/pages/HomePage.js
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 import SupportAnimation from '../components/SupportAnimation';
 import TypedText from '../components/TypedText';
 import Button from '../components/Button';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 import ContributorsStory from '../components/ContributorStory';
 
 const HomePage = () => {
@@ -13,17 +13,17 @@ const HomePage = () => {
     <div className="min-h-screen bg-grid">
       <Header />
       {/* Main Content Section */}
-      <div className="flex flex-col items-center justify-center lg:flex-row">
-        <div className="text-5xl text-purple-900 mb-4 px-4 md:px-0">
-          <div className="w-full text-left md:max-w-3xl">
-            <h1 className="font-dancing text-5xl md:text-8xl">Welcome to</h1>
+      <div className="flex flex-row items-center justify-center flex-wrap px-4 md:px-8">
+        <div className="text-3xl md:text-5xl text-purple-900 mb-4 w-full md:w-auto text-center md:text-left">
+          <div className="w-full md:max-w-3xl">
+            <h1 className="font-dancing text-3xl md:text-8xl">Welcome to</h1>
             <div
-              className="text-4xl sm:text-5xl md:text-6xl inline-block px-4 py-2 rounded-lg font-bold bg-gradient-to-r from-sky-400 via-purple-500 to-pink-300 text-transparent bg-clip-text"
+              className="text-2xl sm:text-3xl md:text-6xl inline-block px-2 py-1 rounded-lg font-bold bg-gradient-to-r from-sky-400 via-purple-500 to-pink-300 text-transparent bg-clip-text"
               style={{ fontFamily: "'Doodle Shadow', cursive" }}
             >
               <TypedText />
             </div>
-            <div className="flex flex-col items-start space-y-4 mt-8 ml-4">
+            <div className="flex flex-col items-center md:items-start space-y-4 mt-6">
               <Button
                 label="About Us"
                 onClick={() => window.scrollTo(0, document.body.scrollHeight)}
@@ -36,7 +36,7 @@ const HomePage = () => {
           </div>
         </div>
         {/* Animation */}
-        <div className="min-h-screen flex items-center justify-start px-8 md:pl-30">
+        <div className="w-full md:w-auto flex justify-center md:justify-start mt-4 md:mt-0">
           <SupportAnimation />
         </div>
       </div>
