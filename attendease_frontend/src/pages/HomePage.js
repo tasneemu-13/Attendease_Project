b9a7eb45d82343a10a8fedb4e4b7e6df75d590e1
tasneemu-13 <168448640+tasneemu-13@users.noreply.github.com>
2025-08-24 21:24:13 +0530

    Updated HomePage.js for responsiveness

--- attendease_frontend/src/pages/HomePage.js
+++ attendease_frontend/src/pages/HomePage.js
@@ -10,24 +10,19 @@ const HomePage = () => {
   const navigate = useNavigate();
 
   return (
-
     <div className="min-h-screen bg-grid">
       <Header />
       {/* Main Content Section */}
-      <div className="flex items-center justify-center">
-        <div className="text-5xl text-purple-900 mb-4">
-          <div className="w-full text-left" style={{ paddingRight: '450px' }}>
-            <h1 className="font-dancing text-8xl">Welcome to</h1>
-
-            {/* TypedText gradient container */}
+      <div className="flex flex-col items-center justify-center lg:flex-row">
+        <div className="text-5xl text-purple-900 mb-4 px-4 md:px-0">
+          <div className="w-full text-left md:max-w-3xl">
+            <h1 className="font-dancing text-5xl md:text-8xl">Welcome to</h1>
             <div
-              className="text-6xl inline-block px-4 py-2 rounded-lg font-bold bg-gradient-to-r from-sky-400 via-purple-500 to-pink-300 text-transparent bg-clip-text"
+              className="text-4xl sm:text-5xl md:text-6xl inline-block px-4 py-2 rounded-lg font-bold bg-gradient-to-r from-sky-400 via-purple-500 to-pink-300 text-transparent bg-clip-text"
               style={{ fontFamily: "'Doodle Shadow', cursive" }}
             >
               <TypedText />
             </div>
-
-           
             <div className="flex flex-col items-start space-y-4 mt-8 ml-4">
               <Button
                 label="About Us"
@@ -40,16 +35,13 @@ const HomePage = () => {
             </div>
           </div>
         </div>
-
         {/* Animation */}
-        <div className="min-h-screen flex items-center justify-start pl-30">
+        <div className="min-h-screen flex items-center justify-start px-8 md:pl-30">
           <SupportAnimation />
         </div>
       </div>
-
       {/* Contributors */}
       <ContributorsStory />
-
       {/* About Us */}
       <AboutUs />
     </div>
