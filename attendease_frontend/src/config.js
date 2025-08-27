dd1f66fc172f2048d11e42e979dc60ca625852ff
tasneemu-13 <168448640+tasneemu-13@users.noreply.github.com>
2025-08-27 21:39:38 +0530

    Updated config.js for react app to connect backend

--- attendease_frontend/src/config.js
+++ attendease_frontend/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/attendease_backend";
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/attendease_backend";
 
 // For login/register (no /api)
 export const AUTH_BASE_URL = BASE_URL;
