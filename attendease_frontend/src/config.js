65190eb337c31dbfb74e091d948f19591cf47351
tasneemu-13 <168448640+tasneemu-13@users.noreply.github.com>
2025-08-27 11:46:13 +0530

    Create config.js
    Created in order to make navigation of pages dynamic

---
+++ attendease_frontend/src/config.js
@@ -0,0 +1,7 @@
+// src/config.js
+
+// Base URL for your backend API
+const API_BASE_URL =
+  process.env.REACT_APP_API_URL || "http://localhost:8080/attendease_backend/api";
+
+export default API_BASE_URL;
