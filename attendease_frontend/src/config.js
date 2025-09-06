b36542574417cf09b4b22b19a6cdafafe42c0004
tasneemu-13 <168448640+tasneemu-13@users.noreply.github.com>
2025-09-06 20:11:40 +0530

    Update config.js

--- attendease_frontend/src/config.js
+++ attendease_frontend/src/config.js
@@ -1,7 +1,5 @@
 // src/config.js
-
-const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/attendease_backend";
-
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
 // For login/register (no /api)
 export const AUTH_BASE_URL = BASE_URL;
 
